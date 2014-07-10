92262c1d621a5b1f3c12f0b00007baaef4a2b2ea
Robert Stevens <Robert2411@gmail.com>
2014-07-10 20:53:16 +0200

    a working mutex inplementation

--- multithreading/multithreading/main.cpp
+++ multithreading/multithreading/main.cpp
@@ -1,16 +1,25 @@
 #include "../../thread.h"
+#include "../../mutex.h"
 #include <stdio.h>
 int test(void* test)
 {
-	printf("test\r\n");
+	Mutex<int>* mutex = (Mutex<int>*) test;
+	int var = mutex->GetVar();
+	printf("test %d\r\n", var);
 	Sleep (100);
 	return 0;
 }
 int main()
 {
+	Mutex<int> mutex;
 	Thread thread;
 	thread.SetThreadFunction(&test);
+	thread.SetParam(&mutex);
+	mutex.Lock();
+	mutex.SetVar(100);
 	thread.Start();
+	Sleep(1000);
+	mutex.Unlock();
 	Sleep(1000);
 	thread.Stop();
 
---
+++ mutex.h
@@ -0,0 +1,95 @@
+/**************************************************************************************\
+									mutex.h
+
+Author		:	R.J.H.M.	Stevens
+Begin		:	10-07-2014
+				
+Version		:	1.0.0
+
+Description	:	This is a basic mutex class that supports multiple platforms
+
+Changes		:
+				10-07-2014	-	Setting up the basic structor of the class		
+
+\**************************************************************************************/
+
+#ifndef MUTEX_H_
+#define MUTEX_H_
+
+#include <windows.h>
+#include "mutex_errors.h"
+
+template <class T>
+class Mutex
+{
+protected:
+	T m_variable;
+	int m_lastError;
+	HANDLE m_mutex;
+
+public:
+	Mutex()
+	{
+		m_lastError = 0;
+		m_mutex = CreateMutex( 
+			NULL,              // default security attributes
+			FALSE,             // initially not owned
+			NULL);             // unnamed mutex
+	};	
+	~Mutex()
+	{
+	};
+	int Lock()
+	{
+		DWORD dwWaitResult;
+		dwWaitResult = WaitForSingleObject( 
+				m_mutex,    // handle to mutex
+				INFINITE);  // no time-out interval
+
+		if (dwWaitResult == WAIT_OBJECT_0 )
+			return 0;
+
+		if (dwWaitResult == WAIT_ABANDONED)
+		{
+			m_lastError = MUTEX_ERROR_LOCK_WAIT_ABANDONED;
+			return MUTEX_ERROR_LOCK_WAIT_ABANDONED;
+		}
+
+		m_lastError = MUTEX_ERROR_LOCK_ERROR;
+		return MUTEX_ERROR_LOCK_ERROR;
+
+	};
+	int Unlock()
+	{
+		if (! ReleaseMutex(m_mutex))
+		{
+			m_lastError = MUTEX_ERROR_CANT_RELEASE_MUTEX;
+			return MUTEX_ERROR_CANT_RELEASE_MUTEX;
+		}
+		return 0;
+	};
+	
+	T GetVar()
+	{
+		T out;
+		if(Lock())
+			return out;
+		out = m_variable;
+		Unlock();
+		return out;
+	};
+	int SetVar(T variable)
+	{
+		if(Lock())
+			return m_lastError;;
+
+		m_variable = variable;
+
+		if (Unlock())
+			return m_lastError;
+		return 0;
+	};
+		T* GetVarPointer();
+};
+
+#endif
